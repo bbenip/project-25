6958371e16634f323d65eb91fbbd07b7ff0f0531
Baljinder Benipal <bs2benip@edu.uwaterloo.ca>
2019-10-07 19:00:19 -0400

    Generate grid with randomized colors

--- js/day3.js
+++ js/day3.js
@@ -1,1 +1,55 @@
-window.onload = () => {};
+const COLORS = [
+  "BLUE",
+  "PURPLE",
+  "YELLOW",
+  "RED",
+  "GREEN",
+  "PINK"
+];
+
+const COLOR_VALUE_MAP = {
+  BLUE:   "rgb(70, 177, 226)",
+  PURPLE: "rgb(96, 92, 168)",
+  YELLOW: "rgb(243, 246, 29)",
+  RED:    "rgb(220, 74, 32)",
+  GREEN:  "rgb(126, 157, 30)",
+  PINK:   "rgb(237, 112, 161)"
+};
+
+const GRID_DIMENSION = 12;
+
+const gridData = new Array(GRID_DIMENSION);
+for (let i = 0; i < GRID_DIMENSION; ++i) {
+  gridData[i] = new Array(GRID_DIMENSION);
+}
+
+function resetGrid() {
+  for (let i = 0; i < GRID_DIMENSION; ++i) {
+    for (let j = 0; j < GRID_DIMENSION; ++j) {
+      gridData[i][j] = COLORS[
+        Math.floor(Math.random() * COLORS.length)
+      ];
+    }
+  }
+}
+
+window.onload = () => {
+  resetGrid();
+  
+  const table = document.createElement("table");
+
+  for (let i = 0; i < GRID_DIMENSION; ++i) {
+    const row = document.createElement("tr");
+
+    for (let j = 0; j < GRID_DIMENSION; ++j) {
+      const cell = document.createElement("td");
+      cell.style.backgroundColor = COLOR_VALUE_MAP[gridData[i][j]];
+      row.appendChild(cell);
+    }
+
+    table.appendChild(row);
+  }
+
+  const game = document.getElementById("game");
+  game.appendChild(table);
+};
