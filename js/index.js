e19775b79740c4a9cbf5db5ace5b3d838071a733
Baljinder Benipal <bs2benip@edu.uwaterloo.ca>
2019-10-20 21:18:17 -0400

    Update project statement

--- js/index.js
+++ js/index.js
@@ -8,11 +8,11 @@ const projects = [
 
 window.onload = () => {
   projects.reverse().forEach((project, index) => {
-    const day = projects.length - index;
-    const file = "day" + day + ".html";
+    const number = projects.length - index;
+    const file = "game" + number + ".html";
 
     const link = "<a href=\"" + file + "\">" + project + "</a>";
-    const entry = "<p>Day " + day + ": " + link + "</p>";
+    const entry = "<p>Game " + number + ": " + link + "</p>";
 
     document.getElementById("project-list").innerHTML += entry;
   });
