b970541b61583f9d2d7151777f2825ee2f971afe
Baljinder Benipal <bbenipal10@gmail.com>
2020-08-26 15:51:49 -0400

    Validate moves

--- js/game7.js
+++ js/game7.js
@@ -16,7 +16,7 @@ const DIRECTIONS = [
   [1, 1],
 ];
 
-const board = [
+const BOARD = [
   [0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0],
@@ -27,13 +27,55 @@ const board = [
   [0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
-let pieceCurrent;
+let pieceCurrent = PIECE_INITIAL;
+let validMoves = [];
+
+function getValidMoves() {
+  const moves = [];
+
+  for (let i = 0; i < GRID_DIMENSION; ++i) {
+    for (let j = 0; j < GRID_DIMENSION; ++j) {
+      if (BOARD[i][j] !== pieceCurrent) continue;
+
+      for (const direction of DIRECTIONS) {
+        let i2 = i + direction[0];
+        let j2 = j + direction[1];
+        let visitedOppositePieces = false;
+
+        while (isValidCoordinate(i2, j2)) {
+          if (BOARD[i2][j2] !== getOppositePiece(BOARD[i][j])) {
+            if (BOARD[i2][j2] === PIECE_BLANK && visitedOppositePieces) {
+              moves.push([i2, j2]);
+            }
+
+            break;
+          }
+
+          i2 += direction[0];
+          j2 += direction[1];
+          visitedOppositePieces = true;
+        }
+      }
+    }
+  }
+
+  return moves;
+}
+
+function resetGame() {
+  pieceCurrent = PIECE_INITIAL;
+  validMoves = getValidMoves();
+}
 
 function cellToCoordinate(cell) {
   const i = ~~(cell / GRID_DIMENSION);
   const j = cell % GRID_DIMENSION;
 
   return { i, j };
+}
+
+function isValidMove(i, j) {
+  return validMoves.some(([i2, j2]) => i2 === i && j2 === j);
 }
 
 function isValidCoordinate(i, j) {
@@ -55,19 +97,19 @@ function swapPieces(i, j, i2, j2) {
     i += direction[0];
     j += direction[1];
 
-    board[i][j] = getOppositePiece(board[i][j]);
+    BOARD[i][j] = getOppositePiece(BOARD[i][j]);
   }
 }
 
 function captureCells(i, j) {
-  for (direction of DIRECTIONS) {
+  for (const direction of DIRECTIONS) {
     let i2 = i + direction[0];
     let j2 = j + direction[1];
 
     while (isValidCoordinate(i2, j2)) {
-      if (board[i2][j2] === PIECE_BLANK) break;
+      if (BOARD[i2][j2] === PIECE_BLANK) break;
 
-      if (board[i2][j2] === pieceCurrent) {
+      if (BOARD[i2][j2] === pieceCurrent) {
         swapPieces(i, j, i2, j2);
         break;
       }
@@ -82,37 +124,34 @@ function addPiece(event) {
   const cell = event.target.id.match(/cell(.*)/)[1];
   const { i, j } = cellToCoordinate(cell);
 
-  if (board[i][j] !== PIECE_BLANK) {
+  if (!isValidMove(i, j)) {
     return;
   }
 
-  board[i][j] = pieceCurrent;
+  BOARD[i][j] = pieceCurrent;
   captureCells(i, j);
 
   pieceCurrent = getOppositePiece(pieceCurrent);
+  validMoves = getValidMoves();
 
   renderGame();
-}
-
-function resetGame() {
-  pieceCurrent = PIECE_INITIAL;
 }
 
 function renderGame() {
   for (let i = 0; i < GRID_DIMENSION; ++i) {
     for (let j = 0; j < GRID_DIMENSION; ++j) {
-      if (board[i][j] === 0) {
+      if (BOARD[i][j] === 0) {
         continue;
       }
 
       const cellNumber = j + i * GRID_DIMENSION;
       const piece = document.getElementById('piece' + cellNumber);
 
-      if (board[i][j] === PIECE_BLANK) {
+      if (BOARD[i][j] === PIECE_BLANK) {
         piece.className = 'piece blank';
-      } else if (board[i][j] === PIECE_BLACK) {
+      } else if (BOARD[i][j] === PIECE_BLACK) {
         piece.className = 'piece black';
-      } else if (board[i][j] === PIECE_WHITE) {
+      } else if (BOARD[i][j] === PIECE_WHITE) {
         piece.className += 'piece white';
       }
     }
@@ -120,7 +159,7 @@ function renderGame() {
 }
 
 window.onload = () => {
-  const board = document.createElement('table');
+  const table = document.createElement('table');
 
   for (let i = 0; i < GRID_DIMENSION; ++i) {
     const row = document.createElement('tr');
@@ -139,11 +178,11 @@ window.onload = () => {
       row.appendChild(cell);
     }
 
-    board.appendChild(row);
+    table.appendChild(row);
   }
 
   const gameContainer = document.getElementById('game');
-  gameContainer.appendChild(board);
+  gameContainer.appendChild(table);
 
   resetGame();
   renderGame();
