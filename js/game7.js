f1eb9fb53966934d6d3eba463b6c1c9c7674f0b6
Baljinder Benipal <bbenipal10@gmail.com>
2020-08-25 23:11:24 -0400

    Capture surrounded pieces

--- js/game7.js
+++ js/game7.js
@@ -5,6 +5,17 @@ const PIECE_INITIAL = 1;
 const PIECE_BLACK = 1;
 const PIECE_WHITE = 2;
 
+const DIRECTIONS = [
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, -1],
+  [0, 1],
+  [1, -1],
+  [1, 0],
+  [1, 1],
+];
+
 const board = [
   [0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0],
@@ -25,10 +36,50 @@ function cellToCoordinate(cell) {
   return { i, j };
 }
 
+function isValidCoordinate(i, j) {
+  return (
+    i >= 0 && i < GRID_DIMENSION
+    && j >= 0 && j < GRID_DIMENSION
+  );
+}
+
+function getOppositePiece(piece) {
+  return (piece === PIECE_BLANK) ? piece : piece % 2 + 1;
+}
+
+function swapPieces(i, j, i2, j2) {
+  const length = Math.max(Math.abs(i2 - i), Math.abs(j2 - j));
+  const direction = [(i2 - i) / length, (j2 - j) / length];
+
+  for (let n = 0; n < length - 1; ++n) {
+    i += direction[0];
+    j += direction[1];
+
+    board[i][j] = getOppositePiece(board[i][j]);
+  }
+}
+
+function captureCells(i, j) {
+  for (direction of DIRECTIONS) {
+    let i2 = i + direction[0];
+    let j2 = j + direction[1];
+
+    while (isValidCoordinate(i2, j2)) {
+      if (board[i2][j2] === PIECE_BLANK) break;
+
+      if (board[i2][j2] === pieceCurrent) {
+        swapPieces(i, j, i2, j2);
+        break;
+      }
+
+      i2 += direction[0];
+      j2 += direction[1];
+    }
+  }
+}
+
 function addPiece(event) {
   const cell = event.target.id.match(/cell(.*)/)[1];
-  if (cell === null) return;
-
   const { i, j } = cellToCoordinate(cell);
 
   if (board[i][j] !== PIECE_BLANK) {
@@ -36,10 +87,13 @@ function addPiece(event) {
   }
 
   board[i][j] = pieceCurrent;
-  pieceCurrent = pieceCurrent % 2 + 1;
+  captureCells(i, j);
+
+  pieceCurrent = getOppositePiece(pieceCurrent);
 
   renderGame();
 }
+
 function resetGame() {
   pieceCurrent = PIECE_INITIAL;
 }
@@ -51,24 +105,16 @@ function renderGame() {
         continue;
       }
 
-      const coordinate = j + i * GRID_DIMENSION
-      const cell = document.getElementById('cell' + coordinate);
+      const cellNumber = j + i * GRID_DIMENSION;
+      const piece = document.getElementById('piece' + cellNumber);
 
-      if (cell.hasChildNodes()) {
-        continue;
-      }
-
-      const piece = document.createElement('div');
-      piece.id = 'cell' + coordinate;
-      piece.className = 'piece';
-
-      if (board[i][j] === PIECE_BLACK) {
-        piece.className += ' black';
+      if (board[i][j] === PIECE_BLANK) {
+        piece.className = 'piece blank';
+      } else if (board[i][j] === PIECE_BLACK) {
+        piece.className = 'piece black';
       } else if (board[i][j] === PIECE_WHITE) {
-        piece.className += ' white';
+        piece.className += 'piece white';
       }
-
-      cell.appendChild(piece);
     }
   }
 }
@@ -81,8 +127,15 @@ window.onload = () => {
 
     for (let j = 0; j < GRID_DIMENSION; ++j) {
       const cell = document.createElement('td');
-      cell.id = 'cell' + (j + i * GRID_DIMENSION);
+      const piece = document.createElement('div');
+
+      const cellNumber = j + i * GRID_DIMENSION;
+      cell.id = 'cell' + cellNumber;
+      piece.id = 'piece' + cellNumber;
+
       cell.addEventListener('click', addPiece);
+
+      cell.appendChild(piece);
       row.appendChild(cell);
     }
 
