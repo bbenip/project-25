639c9b40aff05a1f3594f353627b8b7f4881a203
Baljinder Benipal <bbenipal10@gmail.com>
2020-08-31 22:15:36 -0400

    Add ground

--- js/flappy-bird.js
+++ js/flappy-bird.js
@@ -3,21 +3,35 @@ let context;
 const GAME_HEIGHT = 2000;
 const GAME_WIDTH = 1000;
 
-const GRAVITY = 4;
+const GRAVITY = 2;
 
-const PLAYER_X_LENGTH = 50;
-const PLAYER_Y_LENGTH = 50;
+const PLAYER_WIDTH = 50;
+const PLAYER_HEIGHT = 50;
 
 const PLAYER_INIT_X = 250;
 const PLAYER_INIT_Y = 500;
 const PLAYER_INIT_Y_VELOCITY = 0;
 
+const GROUND_HEIGHT = 100;
+
 const player = {
   x: PLAYER_INIT_X,
   y: PLAYER_INIT_Y,
 
+  width: PLAYER_WIDTH,
+  height: PLAYER_HEIGHT,
+
   yVelocity: PLAYER_INIT_Y_VELOCITY,
-}
+};
+
+const ground = {
+  x: 0,
+  y: GAME_HEIGHT - GROUND_HEIGHT,
+
+  width: GAME_WIDTH,
+  height: GROUND_HEIGHT,
+};
+
 
 function resetGame() {
   player.y = PLAYER_INIT_Y;
@@ -27,28 +41,34 @@ function resetGame() {
 function renderGame() {
   // Draw board
   context.fillStyle = 'black';
-  context.fillRect(
-    0,
-    0,
-    GAME_WIDTH,
-    GAME_HEIGHT
-  );
+  context.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
 
   // Draw player
   context.fillStyle = 'red';
-  context.fillRect(
-    player.x,
-    player.y,
-    PLAYER_X_LENGTH,
-    PLAYER_Y_LENGTH,
-  );
+  context.fillRect(player.x, player.y, player.width, player.height);
+
+  // Draw ground
+  context.fillStyle = 'grey';
+  context.fillRect(ground.x, ground.y, ground.width, ground.height);
+}
+
+function playerHitGround() {
+  return player.y + player.height >= ground.y;
 }
 
 function play() {
   player.yVelocity += GRAVITY;
-  player.y += player.yVelocity;
+  player.y = Math.min(
+    player.y + player.yVelocity, ground.y - player.height / 2
+  );
 
   renderGame();
+
+  if (playerHitGround()) {
+    alert('Game over!');
+    resetGame();
+  }
+
 }
 
 window.onload = () => {
@@ -58,6 +78,6 @@ window.onload = () => {
   resetGame();
   renderGame();
 
-  const refreshRate = 40;
+  const refreshRate = 20;
   setInterval(play, refreshRate);
 };
