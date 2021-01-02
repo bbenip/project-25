6c0b5b38387c619a124fb3a5f2e18504ef240d33
Baljinder Benipal <bbenipal10@gmail.com>
2021-01-02 12:28:56 -0500

    Refactor project list rendering

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 const projects = [
-  'Tetris',
-  'Tron',
-  'Connect 4',
-  'Flappy Bird',
-  'Reversi',
-  '2048',
-  'Sliding Puzzle',
-  'Snake',
-  'Flood-It!',
-  'Pong',
   'Tic-Tac-Toe',
+  'Pong',
+  'Flood-It!',
+  'Snake',
+  'Sliding Puzzle',
+  '2048',
+  'Reversi',
+  'Flappy Bird',
+  'Connect 4',
+  'Tron',
+  'Tetris',
 ];
 
 function getFilename(project) {
@@ -18,19 +18,22 @@ function getFilename(project) {
   filename = filename.replace(/!/g, '');
 
   if (!isNaN(filename)) {
-    filename = 'game' + filename;
+    filename = `game${filename}`;
   }
 
-  return filename;
+  return `${filename}.html`;
 }
 
 window.onload = () => {
-  for (const [index, project] of Object.entries(projects)) {
-    const number = projects.length - index;
-    const filename = getFilename(project) + '.html';
-    const link = '<a href="' + filename + '">' + project + '</a>';
-    const entry = '<p>Game ' + number + ': ' + link + '</p>';
+  projects.forEach((project, index) => {
+    const gameHyperlink = document.createElement('a');
+    gameHyperlink.href = getFilename(project);
+    gameHyperlink.textContent = project;
 
-    document.getElementById('project-list').innerHTML += entry;
-  }
+    const gameEntry = document.createElement('p');
+    gameEntry.textContent = `Game ${index + 1}: `;
+    gameEntry.appendChild(gameHyperlink);
+
+    document.querySelector('#project-list').prepend(gameEntry);
+  });
 };
