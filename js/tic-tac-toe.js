355eda50adc7277526fc06c47bd0364b2899d18b
Baljinder Benipal <bbenipal10@gmail.com>
2021-01-03 14:21:36 -0500

    Refactor tic-tac-toe

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -20,40 +20,54 @@ function checkWin() {
   let win = false;
   for (let arrangement of arrangements) {
     const regex = new RegExp(arrangement, 'g');
-    const count = (boardState[piece].match(regex) || []).length
+    const count = (boardState[piece].match(regex) || []).length;
 
-    if (count === 3) win = true;
+    win ||= (count === 3);
   }
 
-  if (win) alert(piece + ' wins!');
-  if (!win && turn === NUM_CELLS) alert('The game ended in a draw!');
-  if (!win && turn !== NUM_CELLS) return;
+  if (!win && turn !== NUM_CELLS) {
+    return;
+  } else if (win) {
+    alert(piece + ' wins!');
+  } else {
+    alert('The game ended in a draw!');
+  }
+
+  turn = 0;
+  boardState = { x: '', o: '' };
 
   for (let i = 0; i < NUM_CELLS; ++i) {
-    turn = 0;
-    document.querySelector(`#cell${i}`).innerHTML = '';
-    boardState = { x: '', o: '' };
+    document.querySelector(`#cell${i}`).textContent = '';
   }
 }
 
-function addPiece() {
-  if (this.innerHTML !== '') return;
+function addPiece(event) {
+  const cell = event.target;
+
+  if (cell.textContent !== '') {
+    return;
+  }
 
   const piece = (turn % NUM_PLAYERS === 0) ? 'o' : 'x';
-  this.innerHTML = piece;
+  cell.textContent = piece;
 
-  const cellNumber = Number(this.id.match(/\d+/)[0]);
+  const cellNumber = Number(cell.id.match(/\d+/)[0]);
   const v = cellNumber % DIMENSION;
   const h = ~~(cellNumber / DIMENSION);
 
   boardState[piece] += 'v' + v;
   boardState[piece] += 'h' + h;
 
-  if (v === h) boardState[piece] += 'd0';
-  if (v + h === DIMENSION - 1) boardState[piece] += 'd1';
+  if (v === h) {
+    boardState[piece] += 'd0';
+  }
+
+  if (v + h === DIMENSION - 1) {
+    boardState[piece] += 'd1';
+  }
 
   checkWin();
-  ++turn;
+  turn += 1;
 }
 
 window.onload = () => {
