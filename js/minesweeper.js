afb50a397de51e30fab78a5f62ca9cd8c2fe0ff1
Baljinder Benipal <bbenipal10@gmail.com>
2021-01-05 14:49:54 -0500

    Enable user input to flag cells

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -1,14 +1,21 @@
 const BOARD_NUM_CELLS_X = 10;
 const BOARD_NUM_CELLS_Y = 10;
 
-const DEFAULT_CELL = -1;
+const CELL_UNEXPOSED = -1;
+const CELL_FLAGGED = -2;
 
+const DEFAULLT_TOTAL_FLAGS = 10;
+const DEFAULT_CELL = CELL_UNEXPOSED;
+
+let totalFlags = DEFAULLT_TOTAL_FLAGS;
 let board = [];
 
 function resetGame() {
   board = Array(BOARD_NUM_CELLS_Y)
     .fill([])
     .map(() => Array(BOARD_NUM_CELLS_X).fill(DEFAULT_CELL));
+
+  totalFlags = DEFAULLT_TOTAL_FLAGS;
 }
 
 function renderBoardDOM() {
@@ -19,6 +26,8 @@ function renderBoardDOM() {
 
     for (let j = 0; j < BOARD_NUM_CELLS_X; ++j) {
       const cell = document.createElement('td');
+      cell.addEventListener('contextmenu', flagCell);
+
       row.appendChild(cell);
     }
 
@@ -26,6 +35,26 @@ function renderBoardDOM() {
   }
 
   document.querySelector('#game').appendChild(board);
+}
+
+function flagCell(event) {
+  event.preventDefault();
+
+  const rightButton = 2;
+
+  if (event.button === rightButton) {
+    const cell = event.target;
+    const x = cell.cellIndex;
+    const y = cell.parentNode.rowIndex;
+
+    if (board[y][x] === CELL_FLAGGED) {
+      board[y][x] = CELL_UNEXPOSED;
+      totalFlags += 1;
+    } else if (board[y][x] === CELL_UNEXPOSED) {
+      board[y][x] = CELL_FLAGGED;
+      totalFlags -= 1;
+    }
+  }
 }
 
 window.onload = () => {
