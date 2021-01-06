dfa551c3a28fe74e4db96c50c7c6a1ee7de772b3
Baljinder Benipal <bbenipal10@gmail.com>
2021-01-05 21:58:36 -0500

    Set mines randomnly across the board

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -3,11 +3,15 @@ const BOARD_NUM_CELLS_Y = 10;
 
 const CELL_UNEXPOSED = -1;
 const CELL_FLAGGED = -2;
+const CELL_UNEXPOSED_MINE = -3;
+
+const MINE_COUNT = 10;
 
 const DEFAULLT_TOTAL_FLAGS = 10;
 const DEFAULT_CELL = CELL_UNEXPOSED;
 
 let totalFlags = DEFAULLT_TOTAL_FLAGS;
+let mines = [];
 let board = [];
 
 function renderBoardDOM() {
@@ -30,12 +34,46 @@ function renderBoardDOM() {
   document.querySelector('#game').appendChild(board);
 }
 
+// Fisher-Yates shuffle
+function shuffle(array) {
+  const shuffledArray = array.slice();
+  const arrayLength = shuffledArray.length;
+
+  for (let i = 0; i < arrayLength - 1; ++i) {
+    const randomIndex = Math.floor(Math.random() * (arrayLength - i));
+    const temp = shuffledArray[arrayLength - 1 - i];
+
+    shuffledArray[arrayLength - 1 - i] = shuffledArray[randomIndex];
+    shuffledArray[randomIndex] = temp;
+  }
+
+  return shuffledArray;
+}
+
+function setMines() {
+  const boardPositions = Array(BOARD_NUM_CELLS_X * BOARD_NUM_CELLS_Y)
+    .fill(CELL_UNEXPOSED)
+    .map((value, index) => index);
+
+  const candidatePositions = shuffle(boardPositions).slice(0, MINE_COUNT);
+
+  mines = candidatePositions.map((position) => ({
+    x: position % BOARD_NUM_CELLS_X,
+    y: ~~(position / BOARD_NUM_CELLS_X),
+  }));
+
+  for (const { x, y } of mines) {
+    board[y][x] = CELL_UNEXPOSED_MINE;
+  }
+}
+
 function resetGame() {
   board = Array(BOARD_NUM_CELLS_Y)
     .fill([])
     .map(() => Array(BOARD_NUM_CELLS_X).fill(DEFAULT_CELL));
 
   totalFlags = DEFAULLT_TOTAL_FLAGS;
+  setMines();
 }
 
 function renderGame() {
@@ -45,7 +83,10 @@ function renderGame() {
         .childNodes[i]
         .childNodes[j];
 
-      if (board[i][j] === CELL_UNEXPOSED) {
+      if (
+        board[i][j] === CELL_UNEXPOSED
+        || board[i][j] === CELL_UNEXPOSED_MINE
+      ) {
         cell.setAttribute('class', 'unexposed');
       } else if (board[i][j] === CELL_FLAGGED) {
         cell.setAttribute('class', 'flagged');
