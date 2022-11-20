cd2785882a59505d676c0e6d27411feca1c9392b
Baljinder Benipal <bbenipal10@gmail.com>
2022-11-20 15:43:28 -0500

    Display Go board

---
+++ js/go.js
@@ -0,0 +1,33 @@
+const BOARD_DIMENSION = 19;
+
+const setupBoardDom = () => {
+  const table = document.createElement('table');
+  table.setAttribute('id', 'board');
+
+  for (let i = 0; i < BOARD_DIMENSION; ++i) {
+    const row = document.createElement('tr');
+
+    for (let j = 0; j < BOARD_DIMENSION; ++j) {
+      const cell = document.createElement('td');
+
+      const lineHorizontal = document.createElement('div');
+      const lineVertical = document.createElement('div');
+
+      lineHorizontal.setAttribute('class', 'line-horizontal');
+      lineVertical.setAttribute('class', 'line-vertical');
+
+      cell.appendChild(lineHorizontal);
+      cell.appendChild(lineVertical);
+
+      row.appendChild(cell);
+    }
+
+    table.appendChild(row);
+  }
+
+  document.querySelector('#game').appendChild(table);
+};
+
+window.onload = () => {
+  setupBoardDom();
+};
